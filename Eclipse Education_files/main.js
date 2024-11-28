7eb8d4f15e2f729faa47336adfbf77fe1d77e09e
ShreyasAndurlekar <and.shreyas@gmail.com>
2024-11-28 23:20:44 +0530

    Homepage 99% Done

--- Eclipse Education_files/main.js
+++ Eclipse Education_files/main.js
@@ -111,7 +111,7 @@ $(function() {
 		autoplay: true,
 		margin: 30,
 		nav: true,
-		navText: [ "<img src='img/prev.png'>", "<img src='img/next.png'>" ]
+		//navText: [ "<img src='img/prev.png'>", "<img src='img/next.png'>" ]
 	});
 
 	// Select all links with hashes
